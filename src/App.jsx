6ea2f82c234ae57a60fcefc547516636da487131
Graham <graham.lim88@gmail.com>
2022-06-17 04:56:23 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TripsAppear from './components/ShowAllTrips';
+
+function App() {
+  return (
+    <div className="App">
+      <TripsAppear/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ShowAllTrips.jsx
@@ -0,0 +1,43 @@
+import { BACKEND_URL } from "./BackendURL.js";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import Container from '@mui/material/Container';
+
+
+export default function TripsAppear() {
+
+  console.log(BACKEND_URL);
+
+  const [allTrips, setAllTrips] = useState([]);
+
+  async function getAllTrips(){
+    try {
+      const results = await axios.get(`${BACKEND_URL}/trips`);
+      const {data} = results;
+      console.log(data);
+      const newArray = [];
+      for (let i = 0; i < data.length; i++) {
+        newArray.push(data[i]);
+      }
+      setAllTrips(newArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getAllTrips();
+  }, []);
+
+
+  return (
+    <div>
+      <br></br>
+      <Container maxWidth ="md">
+        <h3>All Trips</h3>
+        <br></br>
+        {allTrips}
+      </Container>
+    </div>
+  );
+}
